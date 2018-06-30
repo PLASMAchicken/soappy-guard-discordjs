876f32e21be3684f48fc421abe1a6817899e3ebd
PLASMAchicken <plasmachickenmc@gmail.com>
2018-06-30 16:16:32 +0200

    cleanup

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use('/eval', evalRouter)
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
