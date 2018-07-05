0982193e6c87e2452c1f321e8c02147580cc3e93
PLASMAchicken <plasmachickenmc@gmail.com>
2018-07-05 12:45:27 +0200

    added sharding test script

---
+++ sharding.js
@@ -0,0 +1,7 @@
+
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager(`${__dirname}/bot.js`, { totalShards: 1 });
+
+manager.spawn();
+manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
+
