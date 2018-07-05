95940544920c880c444061e24850811c3490afb3
PLASMAchicken <plasmachickenmc@gmail.com>
2018-07-05 21:10:27 +0200

    fixed pool command

--- commands/pool.js
+++ commands/pool.js
@@ -1,43 +1,42 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const tempmessage = require('../utils/tempmessage.js');
-const colorer = require("../config/color.json");
-const botconfig = require("../config/botconfig.json");
-const agree = "✅";
-const disagree = "❎";
+const colorer = require('../config/color.json');
+const agree = '✅';
+const disagree = '❎';
 
 
-let poolembed = new Discord.RichEmbed()
-        .setTitle("Pool")
-        .setFooter(`Use ${agree} or ${disagree} reactions to vote`)
-        .setColor(colorer.pool);
-
-
+const poolembed = new Discord.RichEmbed()
+	.setTitle('Pool')
+	.setFooter(`Use ${agree} or ${disagree} reactions to vote`)
+	.setColor(colorer.pool);
 
 
 module.exports.run = async (bot, message, args) => {
-    var pool = args.join(' ');
-  if (!pool) return tempmessage(message, "Please provide a pool to vote on!");
-    poolembed.setDescription(pool);
+	const pool = args.join(' ');
+	if (!pool) return tempmessage(message, 'Please provide a pool to vote on!');
+	poolembed.setDescription(pool);
 
-   let msg = await message.channel.send(poolembed);
-   await msg.react(agree);
-   await msg.react(disagree);
+	const msg = await message.channel.send(poolembed);
+	await msg.react(agree);
+	await msg.react(disagree);
+	const filter = (reaction) => reaction.emoji.name == agree || reaction.emoji.name == disagree;
+	const reactions = await msg.awaitReactions(filter, { time: 16000 });
+	const reactionsa = reactions;
+	let agreec, disagreec;
+	if(reactions.get(agree)) {agreec = await reactions.get(agree).count - 1;}
+	else{agreec = 0;}
+	if(reactions.get(disagree)) {disagreec = await reactionsa.get(disagree).count - 1;}
+	else{disagreec = 0;}
 
-   const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: 16000});
-
-var agreec = reactions.get(agree).count-1;
-var disagreec = reactions.get(disagree).count-1;
-
-   poolembed.setDescription(`Voting complete! \n\n${agree}: ${agreec}\n${disagree}: ${disagreec}`);
-    msg.edit(poolembed);
-    msg.clearReactions();
+	poolembed.setDescription(`Voting complete! \n\n${agree}: ${agreec}\n${disagree}: ${disagreec}`);
+	msg.edit(poolembed);
+	msg.clearReactions();
 
 
-
-}
+};
 module.exports.help = {
-    name: "pool",
-    description: "Creates a Pool for People to vote on!",
-    usage: "pool <pool>",
-    disableindm: true
-}
+	name: 'pool',
+	description: 'Creates a Pool for People to vote on!',
+	usage: 'pool <pool>',
+	disableindm: true,
+};
