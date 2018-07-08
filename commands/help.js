9e9f08491353f338cbad15612f7f648a3767ec10
PLASMAchicken <plasmachickenmc@gmail.com>
2018-07-08 16:02:40 +0200

    reworked help for #1

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,13 @@ const ms = require('ms');
 
 module.exports.run = (bot, message, args, guildConf) => {
 	if(!args.length) return helpembed.send(message);
-	const command = bot.commands.get(args.join(' ').toLowerCase());
+	const cmd = args.join(' ').toLowerCase()
+	const command = bot.commands.get(cmd) || bot.commands.find(commanda => commanda.help.aliases && commanda.help.aliases.includes(cmd));
 	if (!command) return message.reply('that\'s not a valid command!');
 	const helpcmd = [];
 	helpcmd.push(`**Name:** ${command.help.name}`);
 
-	// if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+	if (command.help.aliases) helpcmd.push(`**Aliases:** ${command.help.aliases.join(', ')}`);
 	if (command.help.description) helpcmd.push(`**Description:** ${command.help.description}`);
 	if (command.help.usage) helpcmd.push(`**Usage:** ${guildConf.prefix}${command.help.usage}`);
 	let cooldown;
