0a6524d7800a7d40ca1404fd6c8f1680691bf104
PLASMAchicken <plasmachickenmc@gmail.com>
2018-07-16 16:24:37 +0200

    reworked !request

--- commands/request.js
+++ commands/request.js
@@ -1,20 +1,19 @@
 const Discord = require('discord.js');
 const colorer = require('../config/color.json');
-const botconfig = require('../config/botconfig.json');
 
 module.exports.run = async (bot, message, args) => {
 	const feedback = args.join(' ');
 
 	const feedbackEmbed = new Discord.RichEmbed()
-		.setDescription('Reports')
+		.setDescription('Feedback')
 		.setColor(colorer.feedback)
-		.addField('Requested by', `${message.author} with ID: ${message.author.id}`)
+		.addField('Requested by', `${message.author.tag} with ID: ${message.author.id}`)
 		.addField('Requested on Server:', `${message.guild.name} with ID: ${message.guild.id} `)
 		.addField('Feedback:', feedback)
 		.setTimestamp(message.createdAt);
-	const botownerguild = bot.guilds.get(botconfig.botownerserverid);
+	const botownerguild = bot.guilds.get(process.env.botownerguild);
 	const requestchannel = botownerguild.channels.find('name', 'bot-requests');
-	if(!requestchannel) return message.channel.send('Couldn\'t find #bot-requests channel. This is an issue on the Bot Owner Server');
+	if(!requestchannel) return message.channel.send('Couldn\'t find #bot-requests channel. This is an issue on the Bot Owner Server! You can do !invite to go to the botowners discord!');
 	message.delete().catch();
 	requestchannel.send(feedbackEmbed);
 
