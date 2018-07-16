c1eab83a3f4befabc83889d70a70f9af08700d6a
PLASMAchicken <plasmachickenmc@gmail.com>
2018-07-16 20:41:45 +0200

    fixed !serverconf

--- commands/serverconf.js
+++ commands/serverconf.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const errors = require('../utils/errors.js');
 
 module.exports.run = async (bot, message, args, guildConf) => {
-	const staffRole = message.guild.roles.find('name', guildConf.adminRole);
-	if (!staffRole || !message.member.roles.has(staffRole.id)) return errors.noPerms(message, guildConf.staffRole + 'role');
+	const staffRole = message.guild.roles.find('name', guildConf.staffRole);
+	if (!staffRole || !message.member.roles.has(staffRole.id)) return errors.noPerms(message, guildConf.staffRole + ' role');
 	const sEmbed = new Discord.RichEmbed()
 		.setColor('RANDOM')
 		.setTitle('Guild Configuration:')
