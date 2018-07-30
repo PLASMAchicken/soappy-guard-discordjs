24f8b72a211976de5f5dcc56825abd9f8bc17ea6
PLASMAchicken <plasmachickenmc@gmail.com>
2018-07-30 22:17:13 +0200

    cleaned up pm2 conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,5 @@ module.exports = {
 	apps : [{
 		name      : 'soappy-guard-discordjs-bot',
 		script    : 'start.js',
-		env: {
-			NODE_ENV: 'development',
-		},
-		env_production : {
-			NODE_ENV: 'production',
-		},
 	}],
-
-	deploy : {
-		production : {
-			user : 'node',
-			ref  : 'origin/master',
-			repo : 'git@github.com:PLASMAchicken/soappy-guard-discordjs.git',
-		},
-	},
 };
